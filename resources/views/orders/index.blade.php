0f62b7965ed1d919bd80ce1ac1fcaf82b15e9c1f
ReaganLwasa <69892515+ReaganLwasa@users.noreply.github.com>
2023-01-29 19:03:09 +0300

    second commit

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container-fluid">
     <div class="col-lg-12">
         <div class="row">
-            <div class="col-md-9">
+            <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
                         <h4 style="float: left;">Order Products</h4> <a href="#" style="float: right;"
@@ -32,6 +32,7 @@
                                     <td>1</td>
                                     <td>
                                         <select name="product_id" id="product_id" class="form-control product_id">
+                                            <option value="">Select Item</option>
                                             @foreach ($products as $product)
                                             <option data-price="{{ $product->price }}" value="{{ $product->id }}">
                                                 {{ $product->product_name }}</option>
@@ -40,7 +41,7 @@
                                     </td>
                                     <td>
                                         <input type="number" name="quantity[]" id="quantity"
-                                            class="form-control quantity">
+                                            class="form-control quantity" value="1">
                                     </td>
                                     <td>
                                         <input type="number" name="price[]" id="price" class="form-control price">
@@ -62,13 +63,75 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <div class="card">
                     <div class="card-header">
                         <h4>Total <b class="total"> 0.00 </b></h4>
                     </div>
                     <div class="card-body">
-                        .............
+                        <div class="panel">
+                            <div class="row">
+                                <table class="table table-striped">
+                                    <tr>
+                                        <td>
+                                            <label for="">Customer Name</label>
+                                            <input type="text" name="customer_name" id="" class="form-control">
+                                        </td>
+                                        <td>
+                                            <label for="">Customer Phone</label>
+                                            <input type="number" name="customer_phone" id="" class="form-control">
+                                        </td>
+                                    </tr>
+                                </table>
+
+                                <td>Payment Method<br>
+                                    <div class="" style="margin-left: -120px;">
+                                        <br>
+                                        <span class="radio-item">
+                                            <input type="radio" name="payment_method" id="payment_method" class="true"
+                                                value="cash" checked="checked">
+                                            <label for="payment_method"> <i class="fa fa-money-bill text-success"></i>
+                                                Cash </label>
+                                        </span>
+
+                                        <span class="radio-item">
+                                            <input type="radio" name="payment_method" id="payment_method" class="true"
+                                                value="bank transfer">
+                                            <label for="payment_method"> <i class="fa fa-university text-danger"></i>
+                                                Bank Transfer </label>
+                                        </span>
+
+                                        <span class="radio-item">
+                                            <input type="radio" name="payment_method" id="payment_method" class="true"
+                                                value="credit Card">
+                                            <label for="payment_method"> <i class="fa fa-credit-card text-info"></i>
+                                                Credit Card </label>
+                                        </span>
+                                    </div>
+
+                                </td>
+                                <br>
+                                <br>
+
+                                <br>
+
+                                <td>Payment
+                                    <input type=" number" name="paid_amount" id="paid_amount" class="form-control">
+                                </td>
+                                <td>Returning Change <input type="number" readonly name="balance" id="balance"
+                                        class="form-control">
+                                </td>
+                                <td>
+                                    <button class="btn-primary btn-lg btn-block mt-3">Save</button>
+                                </td>
+                                <td>
+                                    <button class="btn-danger btn-lg btn-block mt-2">Calculator</button>
+                                </td>
+                                <div class="text-center" style="text-align: center">
+                                    <a href="" class="text-danger text-center"> <i class="fa fa-sign-out-alt"></i> </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -141,7 +204,71 @@
     -webkit-transform: translate3d(25%, 0, 0);
     transform: translate3d(25%, 0, 0);
 }
+
+.radio-item input[type="radio"] {
+    visibility: hidden;
+    width: 20px;
+    height: 20px;
+    margin: 0 5px 0 5px;
+    padding: 0;
+    cursor: pointer;
+}
+
+/* before style */
+.radio-item input[type="radio"]:before {
+    position: relative;
+    margin: 4px -25px -4px 0;
+    display: inline-block;
+    visibility: visible;
+    width: 20px;
+    height: 20px;
+    border-radius: 10px;
+    border: 2px inset rgb(150, 150, 150, 0.75);
+    background: radial-gradient(ellipse at top left, rgb(255, 255, 255) 0%,
+            rgb(250, 250, 250) 5%, rgb(230, 230, 230) 95%, rgb(255, 255, 255) 100%);
+    content: '';
+    cursor: pointer;
+}
+
+/* checked after style */
+.radio-item input[type="radio"]:checked:after {
+    position: relative;
+    top: 0;
+    left: 9px;
+    display: inline-block;
+    border-radius: 6px;
+    visibility: visible;
+    width: 12px;
+    height: 12px;
+    background: radial-gradient(ellipse at top left, rgb(240, 255, 220) 0%,
+            rgb(225, 250, 100) 5%, rgb(75, 75, 0) 95%, rgb(25, 100, 0) 100%);
+    content: '';
+    cursor: pointer;
+}
+
+/* after checked */
+
+.radio-item input[type="radio"].true:checked:after {
+    background: radial-gradient(ellipse at top left, rgb(240, 255, 220) 0%,
+            rgb(225, 250, 100) 5%, rgb(75, 75, 0) 95%, rgb(25, 100, 0) 100%);
+}
+
+.radio-item input[type="radio"].false:checked:after {
+    background: radial-gradient(ellipse at top left, rgb(255, 255, 255) 0%,
+            rgb(250, 250, 250) 5%, rgb(230, 230, 230) 95%, rgb(255, 255, 255) 100%);
+}
+
+.radio-item label {
+    display: inline-block;
+    margin: 0;
+    padding: 0;
+    line-height: 25px;
+    height: 25px;
+    cursor: pointer;
+}
 </style>
+
+
 @endsection
 
 @section('script')
@@ -154,9 +281,9 @@ $(document).ready(function() {
         var tr = '<tr><td class"no"">' + numberofrow + '</td>' +
             '<td><select class="form-control product_id" name="product_id[]">' + product +
             '</select></td>' +
-            '<td> <input type="number" name="quantity[]" class="form-control"></td>' +
-            '<td> <input type="number" name="price[]" class="form-control"></td>' +
-            '<td> <input type="number" name="discount[]" class="form-control"></td>' +
+            '<td> <input type="number" name="quantity[]" class="form-control quantity"></td>' +
+            '<td> <input type="number" name="price[]" class="form-control price"></td>' +
+            '<td> <input type="number" name="discount[]" class="form-control discount"></td>' +
             '<td> <input type="number" name="total_amount[]" class="form-control total_amount"></td>' +
             '<td><a class="btn btn-danger btn-sm delete rounded-circle"><i class="fa fa-times-circle"></a></td>'
 
@@ -187,17 +314,20 @@ $(document).ready(function() {
         var disc = tr.find('.discount').val() - 0;
         var price = tr.find('.price').val() - 0;
         var Total_amount = (qty * price) - ((qty * price * disc) / 100);
-        tr.find('.total_amount').val(total_amount);
-    })
+        tr.find('.total_amount').val(Total_amount);
+        TotalAmount();
+    });
 
-    $('.addMoreProduct').delegate('.quantity', 'discount', 'keyup', function() {
+    $('.addMoreProduct').delegate('.quantity, discount', 'keyup', function() {
         var tr = $(this).parent().parent();
         var qty = tr.find('.quantity').val() - 0;
         var disc = tr.find('.discount').val() - 0;
         var price = tr.find('.price').val() - 0;
         var Total_amount = (qty * price) - ((qty * price * disc) / 100);
-        tr.find('.total_amount').val(total_amount);
+        tr.find('.total_amount').val(Total_amount);
+        TotalAmount();
     })
+
 
 })
 </script>
